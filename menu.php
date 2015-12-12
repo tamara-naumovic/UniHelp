ca7b6f2953d30a87d109de82db0289e1c9f904cd
millicica <marjanovicmila94@gmail.com>
2015-12-12 10:18:04 +0100

    Ikonica logout

--- menu.php
+++ menu.php
@@ -27,8 +27,16 @@
           </ul>
 
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="logout.php">Log Out</a></li>
-            
+            <li><a href="logout.php">
+                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAABN0lEQVRIS7WWYVHDQBBGXxUACqAKAAUggSqgDgAFgIKCAxyAhNZBcQAKAAUwL3P3g51Qmtx2ZzKX/Mj3ct/u7WYC7APHwDvwRnJMgHUBVGmfP4FlWX0eDRfwDbwC18AJcFRW7/fChoSdDtlkBdwDdz0vCvM6D+vWjP8AUch8ad/WMRQQhV8Ar6e/iK0AhS+BB+CmD9IKUNPc3Zaqm0ULMwBCLopNH4AQq62LLIBalrX5sLS1q8tLJkA9q0zIWc1LBJgs20ZreG6MZR/ArbaGOzBWu7DouZz8R9tPJsCdK35Q+lpqkufAAvgqJZtaphbGlX4X8V+9qtWi2io6v3fRKtKbXXq7dtgchoEzHXJIag7Muh7WyVWnWNRytA46iAIc7vXkKahIHPr+bYz64xCgr37VaJFNlv0A119hmK89wiQAAAAASUVORK5CYII="/>
+                </a></li>
+            <style type="text/css">
+                                    div.image {
+                                    max-width:            24px;
+                                    max-height:           24px;
+                                    background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAABN0lEQVRIS7WWYVHDQBBGXxUACqAKAAUggSqgDgAFgIKCAxyAhNZBcQAKAAUwL3P3g51Qmtx2ZzKX/Mj3ct/u7WYC7APHwDvwRnJMgHUBVGmfP4FlWX0eDRfwDbwC18AJcFRW7/fChoSdDtlkBdwDdz0vCvM6D+vWjP8AUch8ad/WMRQQhV8Ar6e/iK0AhS+BB+CmD9IKUNPc3Zaqm0ULMwBCLopNH4AQq62LLIBalrX5sLS1q8tLJkA9q0zIWc1LBJgs20ZreG6MZR/ArbaGOzBWu7DouZz8R9tPJsCdK35Q+lpqkufAAvgqJZtaphbGlX4X8V+9qtWi2io6v3fRKtKbXXq7dtgchoEzHXJIag7Muh7WyVWnWNRytA46iAIc7vXkKahIHPr+bYz64xCgr37VaJFNlv0A119hmK89wiQAAAAASUVORK5CYII=');
+                                    }
+                                    </style>
           </ul>
           <!--nesto-->
         </div>
